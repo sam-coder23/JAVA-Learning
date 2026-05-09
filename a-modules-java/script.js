17034390f9fe9994fdf4772c2856b1bc4dd307e9
MD Sharique <md.sharique@barco.com>
2026-05-10 02:52:19 +0530

    Initial commit: Add JAVA modules

---
+++ a-modules-java/script.js
@@ -0,0 +1,12 @@
+// Variable and keywords : Keywords are those which has a special meaning in java and we cannot use them as variable name. 
+// Variable is a name which is used to store data in memory. 
+
+// Variable and Declaration :
+ // let var const 
+ // var is old we don't use anymore 
+ // let is something for eg in cricket i am playing cricket starting from my score = 0 and this this score is going to change whenever i score a run .
+ // const is something which is constant and cannot be changed eg : value of Pie = 3.14
+
+ // Declaration and initialization :
+ // Declaration means ek variable ko naam dediya .
+ // Initialization means ek variable ko value dediya .
