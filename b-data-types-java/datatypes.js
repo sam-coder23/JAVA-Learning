718d8a0473bdd178e7bce5fc3f447a6cb5e82ff0
MD Sharique <md.sharique@barco.com>
2026-05-11 17:59:03 +0530

    Move b-data-types-java to separate folder with datatypes.js

---
+++ b-data-types-java/datatypes.js
@@ -0,0 +1,8 @@
+// DATA TYPES IN JAVA
+// 1. Primitive Data Types : Basically which does'nt contain brackets
+// byte, short, int, long, float, double, char, boolean
+// Inko copy krne pe real values milegi.
+
+// 2. Non-Primitive Data Types (Reference) : Basically which contains brackets
+// String, Array, Class, Interface, Object
+// Inko copy krne pe real values nahi milegi balki unka reference milega.
